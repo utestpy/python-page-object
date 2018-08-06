2d7f48cd1f071bbf87a68a0d003ccd710bf725af
Volodymyr Yahello <vyahello@gmail.com>
2018-08-07 00:22:58 +0300

    Add unittests

--- tests/tests/unittests/browsers/test_browsers.py
+++ tests/tests/unittests/browsers/test_browsers.py
@@ -3,6 +3,7 @@ import pytest
 from web.browsers import WebBrowser, WebBrowserError
 
 
+@pytest.mark.unittest
 @pytest.mark.parametrize("name", [
     'Safari', 'Chrome'
 ])
@@ -10,6 +11,7 @@ def test_browser_name(browser: Callable[[str], WebBrowser], name: str) -> None:
     assert browser(name).name() == name
 
 
+@pytest.mark.unittest
 def test_browser_error():
     with pytest.raises(WebBrowserError):
         raise WebBrowserError('Raised WebBrowserError!')
---
+++ tests/tests/unittests/map/test_urls.py
@@ -0,0 +1,21 @@
+import pytest
+from web.map.urls import Url
+
+_home: str = 'http://newtours.demoaut.com/mercurywelcome.php'
+_register: str = 'http://newtours.demoaut.com/mercuryregister.php'
+_sign_on: str = 'http://newtours.demoaut.com/mercurysignon.php'
+
+
+@pytest.mark.unittest
+def test_home_url(home_url: Url) -> None:
+    assert home_url.get() == _home
+
+
+@pytest.mark.unittest
+def test_register_url(register_url: Url) -> None:
+    assert register_url.get() == _register
+
+
+@pytest.mark.unittest
+def test_sign_on_url(sign_on_url: Url) -> None:
+    assert sign_on_url.get() == _sign_on
